5fe80d765c12753da4bb7f335e3785e3b3f0a334
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-10 16:35:53 +0100

    creado el modelo del usuario

---
+++ back/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bycript = require('bycript');
+
+const userSchema = new mongoose.Schema({
+    nombre: {type: String, required: true, unique: true, trim: true},
+    contrasena: {type: String, required: true, trim: true},
+    rol: {type: String, enum: ['administrador', 'programador'], required: true},
+    tareas: {type: mongoose.Schema.Types.ObjectId, ref: 'tareas'}
+})
+
+userSchema.pre('save', async function (next){
+    const user = this;
+    if (user.isModified('password') || user.isNew) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(user.password, salt);
+            user.password = hash;
+            next();
+        }catch (error){
+            return next(error);
+        }
+    }else{
+        return next();
+    }
+});
+
+userSchema.methods.comparePassword = function (password){
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
