e8bed386c7eac559508cb455955bbf50e5d10542
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-10 19:09:52 +0100

    añadidas las rutas para el usuario

---
+++ back/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/registro', UserController.registro);
+router.post('/login', UserController.inicioSesion);
+router.get('/getUsuario', authMiddleware, UserController.getUsuario);
+router.put('modificar-contrasena', authMiddleware, UserController.modificarContrasena);
+
+module.exports = router;
