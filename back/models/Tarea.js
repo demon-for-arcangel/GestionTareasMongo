f7f68a65571321e95dc7bd750f58def58408c3af
Marina Laguna <marinalaguna2004@gmail.com>
2024-02-10 16:41:46 +0100

    creado el modelo de las tareas

---
+++ back/models/Tarea.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const tareaSchema = new mongoose.Schema({
+    descripcion: {type: String, required: true},
+    dificultad: {type: String, enum: ['XS', 'S', 'M', 'L', 'XL'], required: true},
+    tiempoEstimado: {type: Number, required: true},
+    tiempoActual: {type: Number, required: true},
+    porcentajeRealizado: {type: Number, default: 0},
+    completado: {type: Boolean, default: false},
+    asignado: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+});
+
+module.exports = mongoose.model('Tarea', tareaSchema);
